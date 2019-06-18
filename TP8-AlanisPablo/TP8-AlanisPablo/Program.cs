971373d8b9b60f6e6356c1f6c2fa0417e2cefcbd
Pablo Alanis <alanis_pablo@hotmail.com>
2019-06-18 03:26:53 -0300

    TP terminado

---
+++ TP8-AlanisPablo/TP8-AlanisPablo/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<empleado> listaEmp = new List<empleado>();
+            empleado emp;
+            string ruta = "NuevosEmpleados.csv";
+            string[] lineaJunta;
+            string[] lineaSeparada;
+            lineaJunta = File.ReadAllLines("Empleados.csv");
+            for (int i = 0; i <= lineaJunta.Count(); i++)
+            {
+                lineaSeparada = lineaJunta[i].Split(';');
+                emp = empleadoDeArreglo(lineaSeparada);
+                listaEmp.Add(emp);
+            }
+            for (int i = 0; i <= listaEmp.Count(); i++)
+            {
+                escribirArchivo(ruta, listaEmp[i]);
+            }
+            backUpEmpleados(ruta);
+            Console.ReadKey();
+        }
+
+        public static empleado empleadoDeArreglo(string[] lineaSeparada)
+        {
+            string nombre = lineaSeparada[0];
+            string apellido = lineaSeparada[1];
+            string[] fecha = lineaSeparada[2].Split('/');
+            int dia = int.Parse(fecha[0]);
+            int mes = int.Parse(fecha[1]);
+            int anio = int.Parse(fecha[2]);
+            DateTime fechaNac = new DateTime(anio, mes, dia);
+            EstadoCivil estado;
+
+            if (lineaSeparada[3] == "Casadx")
+            {
+                estado = (EstadoCivil)1;
+            }
+            else
+                if (lineaSeparada[3] == "solterx")
+                {
+                    estado = (EstadoCivil)2;
+                }
+                else
+                {
+                    estado = (EstadoCivil)3;
+                }
+            Genero genero;
+            if (lineaSeparada[4] == "masculino")
+            {
+                genero = (Genero)1;
+            }
+            else
+            {
+                genero = (Genero)2;
+            }
+
+            string[] fecha2 = lineaSeparada[7].Split('/');
+            dia = int.Parse(fecha2[0]);
+            mes = int.Parse(fecha2[1]);
+            anio = int.Parse(fecha2[2]);
+            DateTime fechaIngreso = new DateTime(anio, mes, dia);
+            double sueldo = double.Parse(lineaSeparada[8]);
+            string carg = lineaSeparada[9];
+            cargo Cargo;
+            if (carg == "auxiliar")
+            {
+                Cargo = (cargo)1;
+            }
+            else
+                if (carg == "administrativo")
+                {
+                    Cargo = (cargo)2;
+                }
+                else
+                    if (carg == "ingeniero")
+                    {
+                        Cargo = (cargo)3;
+                    }
+                    else
+                        if (carg == "especialista")
+                        {
+                            Cargo = (cargo)4;
+                        }
+                        else
+                        {
+                            Cargo = (cargo)5;
+                        }
+            int hijos = int.Parse(lineaSeparada[5]);
+            empleado emp = new empleado(nombre, apellido, fechaNac, estado, genero, hijos, fechaIngreso, sueldo, Cargo);
+            return (emp);
+        }
+
+        public static int cantidadDeEmpleados(List<empleado> lista)
+        {
+            int cant = 0;
+            cant = lista.Count;
+            return (cant);
+        }
+
+        public static void escribirArchivo(string ruta, empleado empl)
+        {
+            string fecha;
+            using (StreamWriter file = new StreamWriter(ruta, true))
+            {
+                file.Write(empl.nombre + ";");
+                file.Write(empl.apellido + ";");
+                fecha = Convert.ToDateTime(empl.fechaNac).ToString("dd/MM/yy");
+                file.Write(fecha + ";");
+                file.Write(empl.genero + ";");
+                file.Write(empl.estadoCivil + ";");
+                file.Write(empl.fechaIngreso + ";");
+                fecha = Convert.ToDateTime(empl.fechaIngreso).ToString("dd/MM/yy");
+                file.Write(fecha + ";");
+                file.Write(empl.actual + ";");
+                file.Write(empl.hijos + ";");
+                file.Close();
+            }
+        }
+
+        public static void backUpEmpleados(string ruta)
+        {
+            string[] directorio = System.IO.Directory.GetLogicalDrives();
+            string destino;
+            int opcion = 0;
+            Console.WriteLine("¿en que unidad desea hacer la copia de seguridad? \n1 - {0} \n2 - {1}\n", directorio[0], directorio[4]);
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1: destino = @"C:\BackUp";
+                    break;
+                default: destino = @"H:\BackUp";
+                    break;
+            }
+            if (!Directory.Exists(destino))
+            {
+                Directory.CreateDirectory(destino);
+            }
+            if (!File.Exists(destino + @"\NuevosEmpleados.bk"))
+            {
+                File.Copy(ruta, destino + @"\NuevosEmpleados.bk");
+            }
+            else
+            {
+                Console.WriteLine("Ya existe una copia del archivo en el directorio: {0}", destino);
+            }
+        }
+    }
+}
---
+++ TP8-AlanisPablo/TP8-AlanisPablo/empleado.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP8
+{
+    public enum cargo { auxiliar, administrativo, ingeniero, especialista, investigador };
+    public enum EstadoCivil { casadx, solterx, viudx };
+    public enum Genero { masculino = 1, femenino = 2 };
+
+    class empleado
+    {
+        public string nombre;
+        public string apellido;
+        public DateTime fechaNac;
+        public EstadoCivil estadoCivil;
+        public int hijos;
+        public Genero genero;
+        public DateTime fechaIngreso;
+        public double sueldoBasico;
+        public cargo actual;
+
+        public empleado(string _nombre, string _apellido, DateTime _fechaNac, EstadoCivil _estadoCivil, Genero _genero, int _hijos, DateTime _fechaIngreso, double _sueldoBasico, cargo _actual)
+        {
+            nombre = _nombre;
+            apellido = _apellido;
+            fechaNac = _fechaNac;
+            estadoCivil = _estadoCivil;
+            genero = _genero;
+            hijos = _hijos;
+            fechaIngreso = _fechaIngreso;
+            sueldoBasico = _sueldoBasico;
+            actual = _actual;
+        }
+
+        public void mostrarEmpleado()
+        {
+            Console.Write("nombre = {0}\n", nombre);
+            Console.Write("apellido = {0}\n", apellido);
+            Console.Write("fecha de nacimiento = {0}\n", fechaNac);
+            Console.Write("estado civil = {0}\n", estadoCivil);
+            Console.Write("cantidad de hijos = {0}\n", hijos);
+            Console.Write("genero = {0}\n", genero);
+            Console.Write("fehca de ingreso = {0}\n", fechaIngreso);
+            Console.Write("sueldo  = {0}\n", sueldoBasico);
+            Console.Write("cargo = {0}\n", actual);
+        }
+
+        public static void ingresarEmpleado(List<empleado> lista, empleado nuevo)
+        {
+            lista.Add(nuevo);
+        }
+
+        public static int AntiguedadEmpleado(DateTime fechaIngreso, DateTime hoy)
+        {
+            int antiguedad = 0;
+            antiguedad = hoy.Year - fechaIngreso.Year;
+            return antiguedad;
+        }
+
+        public static int edadEmpleado(DateTime fechaNac, DateTime hoy)
+        {
+            int edad = 0;
+            edad = hoy.Year - fechaNac.Year;
+            return edad;
+        }
+
+        public static int añosParaJubilarse(DateTime fechaNac, DateTime hoy, empleado nuevoEmpleado)
+        {
+            int jub = 0, aux = 0;
+            aux = edadEmpleado(fechaNac, hoy);
+            if (nuevoEmpleado.genero == Genero.masculino)
+            {
+                jub = 65 - aux;
+            }
+            else
+            {
+                jub = 60 - aux;
+            }
+            return (jub);
+        }
+
+        public static double salario(empleado nuevoEmpleado, DateTime hoy, DateTime fechaIngreso)
+        {
+            double salario = 0;
+            double adicional = 0;
+            if (AntiguedadEmpleado(fechaIngreso, hoy) <= 20)
+            {
+                adicional = ((nuevoEmpleado.sueldoBasico * 0.02) * (AntiguedadEmpleado(fechaIngreso, hoy)));
+            }
+            else
+            {
+                adicional = (nuevoEmpleado.sueldoBasico * 1.25);
+            }
+            switch (nuevoEmpleado.actual)
+            {
+                case cargo.ingeniero:
+                case cargo.especialista:
+                    adicional = adicional * 1.50;
+                    break;
+                default:
+                    break;
+            }
+            if (nuevoEmpleado.estadoCivil == EstadoCivil.casadx && nuevoEmpleado.hijos >= 2)
+            {
+                adicional = adicional + 5000;
+            }
+            salario = nuevoEmpleado.sueldoBasico + adicional;
+            return (salario);
+        }
+
+        public static int calcularMD(int aux, DateTime fecha1, DateTime hoy)
+        {
+            if (hoy.Month > fecha1.Month)
+            {
+                aux = aux - 1;
+            }
+            else
+            {
+                if (hoy.Day > fecha1.Day)
+                {
+                    aux = aux - 1;
+                }
+            }
+            return (aux);
+        }
+    }
+}
